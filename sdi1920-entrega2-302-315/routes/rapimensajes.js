05450169ea91ad9b38a2afa27d1aee4b7e225090
jesQM <jezuquema@gmail.com>
2020-05-07 20:00:00 +0200

    Started progress on jquery app

--- sdi1920-entrega2-302-315/routes/rapimensajes.js
+++ sdi1920-entrega2-302-315/routes/rapimensajes.js
@@ -1,8 +1,35 @@
 module.exports = function(app, gestorBD) {
 
-    // pp.post("/api/autenticar/", ()=>{});
-    // TODO; Que la variable usuario sea todo el objeto, para asi poder sacar el id o el nombre según se necesite
-    // Igual que está en req.sesion.usuario
+    app.post("/api/autenticar/", function(req, res) {
+        var seguro = app.get("crypto")
+            .createHmac('sha256', app.get('clave'))
+            .update(req.body.password)
+            .digest('hex');
+
+        var criterio = {
+            email : req.body.email,
+            password : seguro
+        };
+
+        gestorBD.obtenerUsuarios(criterio, function(usuarios) {
+            if (usuarios == null || usuarios.length == 0) {
+                res.status(401);
+                res.json({
+                    autenticado : false
+                })
+            } else {
+                var token = app.get('jwt').sign(
+                    {usuario: criterio.email , tiempo: Date.now()/1000},
+                    "secreto");
+                res.status(200);
+                res.json({
+                    autenticado: true,
+                    token : token
+                });
+            }
+
+        });
+    });
 
     app.post("/api/mensaje/crear", function(req, res) {
         var mensaje = {
