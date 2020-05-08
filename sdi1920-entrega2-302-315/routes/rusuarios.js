8566a6f680160582d897f93f0f0ed3c6a431dafc
Pablo Cañal Suárez <uo264903@uniovi.es>
2020-05-08 17:23:54 +0200

    Added W4 - List users

--- sdi1920-entrega2-302-315/routes/rusuarios.js
+++ sdi1920-entrega2-302-315/routes/rusuarios.js
@@ -1,6 +1,38 @@
 module.exports = function(app, swig, gestorBD) {
+
     app.get("/usuarios", function(req, res) {
-        res.send("ver usuarios");
+        let criterio = {};
+
+        if( req.query.busqueda != null ){
+            criterio = { "nombre" : {$regex : ".*"+req.query.busqueda+".*"} };
+        }
+
+        let pg = parseInt(req.query.pg); // Es un String
+        if (req.query.pg == null) { // Puede no venir el param
+            pg = 1;
+        }
+        gestorBD.obtenerUsuariosPag(criterio, pg , function(usuarios, total) {
+            if (usuarios == null) {
+                res.send("Error al listar");
+            } else {
+                let ultimaPg = total / 4;
+                if (total % 4 > 0) { // Sobran decimales
+                    ultimaPg = ultimaPg + 1;
+                }
+                let paginas = []; // mostrar paginas
+                for (let i = pg-2; i <= pg+2; i++) {
+                    if (i > 0 && i <= ultimaPg) {
+                        paginas.push(i);
+                    }
+                }
+                let respuesta = swig.renderFile('views/busuarios.html', {
+                    usuarios : usuarios,
+                    paginas : paginas,
+                    actual : pg
+                });
+                res.send(respuesta);
+            }
+        });
     });
 
     app.get("/registrarse", function(req, res) {
