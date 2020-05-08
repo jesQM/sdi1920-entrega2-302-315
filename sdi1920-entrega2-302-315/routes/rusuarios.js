51e07c58b32ee0b78f9136f1e5b31586c2061e66
Pablo Cañal Suárez <uo264903@uniovi.es>
2020-05-08 18:45:00 +0200

    W5 - Search in users list

--- sdi1920-entrega2-302-315/routes/rusuarios.js
+++ sdi1920-entrega2-302-315/routes/rusuarios.js
@@ -1,17 +1,21 @@
-module.exports = function(app, swig, gestorBD) {
+module.exports = function (app, swig, gestorBD) {
 
-    app.get("/usuarios", function(req, res) {
+    app.get("/usuarios", function (req, res) {
         let criterio = {};
 
-        if( req.query.busqueda != null ){
-            criterio = { "nombre" : {$regex : ".*"+req.query.busqueda+".*"} };
+        if (req.query.busqueda != null) {
+            criterio = {
+                $or: [{"name": {$regex: ".*" + req.query.busqueda + ".*", $options: 'i'}},
+                    {"surname": {$regex: ".*" + req.query.busqueda + ".*", $options: 'i'}},
+                    {"email": {$regex: ".*" + req.query.busqueda + ".*", $options: 'i'}}]
+            };
         }
 
         let pg = parseInt(req.query.pg); // Es un String
         if (req.query.pg == null) { // Puede no venir el param
             pg = 1;
         }
-        gestorBD.obtenerUsuariosPag(criterio, pg , function(usuarios, total) {
+        gestorBD.obtenerUsuariosPag(criterio, pg, function (usuarios, total) {
             if (usuarios == null) {
                 res.send("Error al listar");
             } else {
@@ -20,29 +24,29 @@ module.exports = function(app, swig, gestorBD) {
                     ultimaPg = ultimaPg + 1;
                 }
                 let paginas = []; // mostrar paginas
-                for (let i = pg-2; i <= pg+2; i++) {
+                for (let i = pg - 2; i <= pg + 2; i++) {
                     if (i > 0 && i <= ultimaPg) {
                         paginas.push(i);
                     }
                 }
                 let respuesta = swig.renderFile('views/busuarios.html', {
-                    usuarios : usuarios,
-                    paginas : paginas,
-                    actual : pg
+                    usuarios: usuarios,
+                    paginas: paginas,
+                    actual: pg
                 });
                 res.send(respuesta);
             }
         });
     });
 
-    app.get("/registrarse", function(req, res) {
+    app.get("/registrarse", function (req, res) {
         let respuesta = swig.renderFile('views/bregistro.html', {});
         res.send(respuesta);
     });
 
-    app.post('/usuario', function(req, res) {
+    app.post('/usuario', function (req, res) {
 
-        if(req.body.password !== req.body.passwordRepeat) {
+        if (req.body.password !== req.body.passwordRepeat) {
             res.redirect("/registrarse?mensaje=Las contraseñas no coinciden&tipoMensaje=alert-danger");
             return;
         }
@@ -52,47 +56,47 @@ module.exports = function(app, swig, gestorBD) {
         let usuario = {
             name: req.body.name,
             surname: req.body.surname,
-            email : req.body.email,
-            password : seguro
+            email: req.body.email,
+            password: seguro
         };
 
         let checkExistingEmail = {
             email: usuario.email
         };
 
-        gestorBD.obtenerUsuarios(checkExistingEmail, function(usuarios) {
-           if(usuarios === null || usuarios.length === 0){
-               gestorBD.insertarUsuario(usuario, function(id) {
-                   if (id == null){
-                       res.redirect("/identificarse?mensaje=Error al registrar usuario");
-                   } else {
-                       res.redirect("/identificarse?mensaje=Nuevo usuario registrado");
-                   }
-               });
-           } else {
-               res.redirect(`/registrarse?mensaje=Ya existe un usuario con el email: ${checkExistingEmail.email} &tipoMensaje=alert-danger`)
-           }
+        gestorBD.obtenerUsuarios(checkExistingEmail, function (usuarios) {
+            if (usuarios === null || usuarios.length === 0) {
+                gestorBD.insertarUsuario(usuario, function (id) {
+                    if (id == null) {
+                        res.redirect("/identificarse?mensaje=Error al registrar usuario");
+                    } else {
+                        res.redirect("/identificarse?mensaje=Nuevo usuario registrado");
+                    }
+                });
+            } else {
+                res.redirect(`/registrarse?mensaje=Ya existe un usuario con el email: ${checkExistingEmail.email} &tipoMensaje=alert-danger`)
+            }
 
         });
     });
 
-    app.get("/identificarse", function(req, res) {
+    app.get("/identificarse", function (req, res) {
         let respuesta = swig.renderFile('views/bidentificacion.html', {});
         res.send(respuesta);
     });
 
-    app.post("/identificarse", function(req, res) {
+    app.post("/identificarse", function (req, res) {
         let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
             .update(req.body.password).digest('hex');
         let criterio = {
-            email : req.body.email,
-            password : seguro
+            email: req.body.email,
+            password: seguro
         }
-        gestorBD.obtenerUsuarios(criterio, function(usuarios) {
+        gestorBD.obtenerUsuarios(criterio, function (usuarios) {
             if (usuarios == null || usuarios.length == 0) {
                 req.session.usuario = null;
                 res.redirect("/identificarse" +
-                    "?mensaje=Email o password incorrecto"+
+                    "?mensaje=Email o password incorrecto" +
                     "&tipoMensaje=alert-danger ");
             } else {
                 req.session.usuario = usuarios[0];
@@ -106,11 +110,10 @@ module.exports = function(app, swig, gestorBD) {
         res.redirect("/identificarse");
     });
 
-    app.get('/whoami',function(req,res){
-        if(req.session && req.session.usuario){
+    app.get('/whoami', function (req, res) {
+        if (req.session && req.session.usuario) {
             res.send(req.session.usuario.email);
-        }
-        else{
+        } else {
             res.send(null);
         }
     });
