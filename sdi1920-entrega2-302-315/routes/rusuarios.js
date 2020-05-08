f5500faa8efa5caa2868534ce4ee0b7f97f0d032
Pablo Cañal Suárez <uo264903@uniovi.es>
2020-05-08 13:56:23 +0200

    Added registration process

--- sdi1920-entrega2-302-315/routes/rusuarios.js
+++ sdi1920-entrega2-302-315/routes/rusuarios.js
@@ -8,6 +8,42 @@ module.exports = function(app, swig, gestorBD) {
         res.send(respuesta);
     });
 
+    app.post('/usuario', function(req, res) {
+
+        if(req.body.password !== req.body.passwordRepeat) {
+            res.redirect("/registrarse?mensaje=Las contraseñas no coinciden&tipoMensaje=alert-danger");
+            return;
+        }
+
+        let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+            .update(req.body.password).digest('hex');
+        let usuario = {
+            name: req.body.name,
+            surname: req.body.surname,
+            email : req.body.email,
+            password : seguro
+        };
+
+        let checkExistingEmail = {
+            email: usuario.email
+        };
+
+        gestorBD.obtenerUsuarios(checkExistingEmail, function(usuarios) {
+           if(usuarios === null || usuarios.length === 0){
+               gestorBD.insertarUsuario(usuario, function(id) {
+                   if (id == null){
+                       res.redirect("/identificarse?mensaje=Error al registrar usuario");
+                   } else {
+                       res.redirect("/identificarse?mensaje=Nuevo usuario registrado");
+                   }
+               });
+           } else {
+               res.redirect(`/registrarse?mensaje=Ya existe un usuario con el email: ${checkExistingEmail.email} &tipoMensaje=alert-danger`)
+           }
+
+        });
+    });
+
     app.get("/identificarse", function(req, res) {
         let respuesta = swig.renderFile('views/bidentificacion.html', {});
         res.send(respuesta);
